3385fbfe7be661e59d0448d8d64e77751410bce6
AltF4 <altf4@phx2600.org>
2017-01-01 15:41:10 -0700

    Add some files for PyPI integration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'melee',
+  packages = ['melee'],
+  version = '0.0.1',
+  description = 'Open API for making your own Smash Bros: Melee AI',
+  author = 'AltF4',
+  author_email = 'altf4@phx2600.org',
+  url = 'https://github.com/altf4/libmelee',
+  download_url = 'https://github.com/altf4/libmelee/tarball/0.0.1',
+  keywords = ['dolphin', 'AI', 'video games'],
+  classifiers = [],
+)
