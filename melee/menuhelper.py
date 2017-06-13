45946ea3ac157e3d9b6c0737991477ad012c9fd3
AltF4 <altf4@phx2600.org>
2017-06-12 21:50:06 -0700

    Move stage wait time from 30 frames to 20

--- melee/menuhelper.py
+++ melee/menuhelper.py
@@ -104,7 +104,7 @@ def choosecharacter(character, gamestate, controller, swag=False, start=False):
     gamestate = The current gamestate
     controller = The controller object to press"""
 def choosestage(stage, gamestate, controller):
-    if gamestate.frame < 30:
+    if gamestate.frame < 20:
         return
     target_x, target_y = 0,0
     if stage == enums.Stage.BATTLEFIELD:
