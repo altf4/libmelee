ff215bb1a9a30365b992256a9a305617d6c60d19
AltF4 <altf4@phx2600.org>
2017-07-24 21:21:52 -0700

    Add full-featured inrange() function  - Will tell you if an attack will land  - And on what frame  - Very useful for dodging attacks

--- melee/framedata.py
+++ melee/framedata.py
@@ -88,8 +88,7 @@ class FrameData:
     def isattack(self, character, action):
         # For each frame...
         for i, frame in self.framedata[character][action].items():
-            if frame['hitbox_1_status'] or frame['hitbox_2_status'] \
-                or frame['hitbox_3_status'] or frame['hitbox_4_status']:
+            if frame['hitbox_1_status'] or frame['hitbox_2_status'] or frame['hitbox_3_status'] or frame['hitbox_4_status']:
                 return True
         return False
 
@@ -128,63 +127,115 @@ class FrameData:
 
         return AttackState.ATTACKING
 
-    # Helper in case you want to use the current character states
-    def inrange_simple(self, attacker, defender, future_frames=0):
-        return self.inrange(attacker.character, attacker.action, attacker.action_frame + future_frames, \
-            attacker.facing, attacker.x, attacker.y, defender.character, defender.x, defender.y)
-
-    # Returns true if the attack specified will hit the defender specified
+    # Returns the frame that the specified attack will hit the defender
+    #   Returns 0 if it won't hit
     # NOTE: This considers the defending character to have a single hurtbox, centered
     #       at the x,y coordinates of the player (adjusted up a little to be centered)
-    def inrange(self, attacker_character, attacker_action, attacker_action_frame, \
-        attacker_facing, attacker_x, attacker_y, defender_character, defender_x, defender_y):
-            attackingframe = self.getframe(attacker_character, attacker_action, attacker_action_frame)
+    def inrange(self, attacker, defender, stage):
+        lastframe = self.lasthitboxframe(attacker.character, attacker.action)
+
+        # Adjust the defender's hurtbox up a little, to be more centered.
+        #   the game keeps y coordinates based on the bottom of a character, not
+        #   their center. So we need to move up by one radius of the character's size
+        defender_size = float(self.characterdata[defender.character]["size"])
+        defender_y = defender.y + defender_size
+
+        # Running totals of how far the attacker will travel each frame
+        attacker_x = attacker.x
+        attacker_y = attacker.y
+
+        onground = attacker.on_ground
+
+        attacker_speed_x = 0
+        if onground:
+            attacker_speed_x = attacker.speed_ground_x_self
+        else:
+            attacker_speed_x = attacker.speed_air_x_self
+        attacker_speed_y = attacker.speed_y_self
+
+        friction = self.characterdata[attacker.character]["Friction"]
+        gravity = self.characterdata[attacker.character]["Gravity"]
+        termvelocity = self.characterdata[attacker.character]["TerminalVelocity"]
+
+        for i in range(attacker.action_frame+1, lastframe+1):
+            attackingframe = self.getframe(attacker.character, attacker.action, i)
             if attackingframe is None:
-                return False
-            if self.attackstate(attacker_character, attacker_action, attacker_action_frame) != AttackState.ATTACKING:
-                return False
-            # Calculate the x and y positions of all 4 hitboxes
-            hitbox_1_x = float(attackingframe["hitbox_1_x"])
-            hitbox_1_y = float(attackingframe["hitbox_1_y"]) + attacker_y
-            hitbox_2_x = float(attackingframe["hitbox_2_x"])
-            hitbox_2_y = float(attackingframe["hitbox_2_y"]) + attacker_y
-            hitbox_3_x = float(attackingframe["hitbox_3_x"])
-            hitbox_3_y = float(attackingframe["hitbox_3_y"]) + attacker_y
-            hitbox_4_x = float(attackingframe["hitbox_4_x"])
-            hitbox_4_y = float(attackingframe["hitbox_4_y"]) + attacker_y
+                continue
 
-            if not attacker_facing:
-                hitbox_1_x *= -1
-                hitbox_2_x *= -1
-                hitbox_3_x *= -1
-                hitbox_4_x *= -1
+            # Figure out how much the attaker will be moving this frame
+            #   Is there any locomotion in the animation? If so, use that
+            locomotion_x = float(attackingframe["locomotion_x"])
+            locomotion_y = float(attackingframe["locomotion_y"])
+            if locomotion_y == 0 and locomotion_x == 0:
+                # There's no locomotion, so let's figure out how the attacker will be moving...
+                #   Are they on the ground or in the air?
+                if onground:
+                    attacker_speed_y = 0
+                    # Slow down the speed by the character's friction, then apply it
+                    if attacker_speed_x > 0:
+                        attacker_speed_x = max(0, attacker_speed_x - friction)
+                    else:
+                        attacker_speed_x = min(0, attacker_speed_x + friction)
+                    attacker_x += attacker_speed_x
+                # If attacker is in tha air...
+                else:
+                    # First consider vertical movement. They will decelerate towards the stage
+                    attacker_speed_y = max(-termvelocity, attacker_speed_y - gravity)
+                    # NOTE Assume that the attacker will keep moving how they currently are
+                    # If they do move halfway, then this will re-calculate later runs
 
-            hitbox_1_x += attacker_x
-            hitbox_2_x += attacker_x
-            hitbox_3_x += attacker_x
-            hitbox_4_x += attacker_x
+                    attacker_y += attacker_speed_y
+                    # Did we hit the ground this frame? If so, let's make some changes
+                    if attacker_y <= 0 and abs(attacker_x) < stages.edgegroundposition(stage):
+                        # TODO: Let's consider A moves that cancel when landing
+                        attacker_y = 0
+                        attacker_speed_y = 0
+                        onground = True
 
-            # Adjust the defender's hurtbox up a little, to be more centered.
-            #   the game keeps y coordinates based on the bottom of a character, not
-            #   their center. So we need to move up by one radius of the character's size
-            defender_size = float(self.characterdata[defender_character]["size"])
-            defender_y = defender_y + defender_size
+                    attacker_x += attacker_speed_x
+            else:
+                attacker_x += locomotion_x
+                attacker_y += locomotion_y
 
-            # Now see if any of the hitboxes are in range
-            distance1 = math.sqrt((hitbox_1_x - defender_x)**2 + (hitbox_1_y - defender_y)**2)
-            distance2 = math.sqrt((hitbox_2_x - defender_x)**2 + (hitbox_2_y - defender_y)**2)
-            distance3 = math.sqrt((hitbox_3_x - defender_x)**2 + (hitbox_3_y - defender_y)**2)
-            distance4 = math.sqrt((hitbox_4_x - defender_x)**2 + (hitbox_4_y - defender_y)**2)
+            if attackingframe['hitbox_1_status'] or attackingframe['hitbox_2_status'] or \
+                    attackingframe['hitbox_3_status'] or attackingframe['hitbox_4_status']:
+                # Calculate the x and y positions of all 4 hitboxes for this frame
+                hitbox_1_x = float(attackingframe["hitbox_1_x"])
+                hitbox_1_y = float(attackingframe["hitbox_1_y"]) + attacker_y
+                hitbox_2_x = float(attackingframe["hitbox_2_x"])
+                hitbox_2_y = float(attackingframe["hitbox_2_y"]) + attacker_y
+                hitbox_3_x = float(attackingframe["hitbox_3_x"])
+                hitbox_3_y = float(attackingframe["hitbox_3_y"]) + attacker_y
+                hitbox_4_x = float(attackingframe["hitbox_4_x"])
+                hitbox_4_y = float(attackingframe["hitbox_4_y"]) + attacker_y
 
-            if distance1 < defender_size + float(attackingframe["hitbox_1_size"]):
-                return True
-            if distance2 < defender_size + float(attackingframe["hitbox_2_size"]):
-                return True
-            if distance3 < defender_size + float(attackingframe["hitbox_3_size"]):
-                return True
-            if distance4 < defender_size + float(attackingframe["hitbox_4_size"]):
-                return True
-            return False
+                # Flip the horizontal hitboxes around if we're facing left
+                if not attacker.facing:
+                    hitbox_1_x *= -1
+                    hitbox_2_x *= -1
+                    hitbox_3_x *= -1
+                    hitbox_4_x *= -1
+
+                hitbox_1_x += attacker_x
+                hitbox_2_x += attacker_x
+                hitbox_3_x += attacker_x
+                hitbox_4_x += attacker_x
+
+                # Now see if any of the hitboxes are in range
+                distance1 = math.sqrt((hitbox_1_x - defender.x)**2 + (hitbox_1_y - defender_y)**2)
+                distance2 = math.sqrt((hitbox_2_x - defender.x)**2 + (hitbox_2_y - defender_y)**2)
+                distance3 = math.sqrt((hitbox_3_x - defender.x)**2 + (hitbox_3_y - defender_y)**2)
+                distance4 = math.sqrt((hitbox_4_x - defender.x)**2 + (hitbox_4_y - defender_y)**2)
+
+                if distance1 < defender_size + float(attackingframe["hitbox_1_size"]):
+                    return i
+                if distance2 < defender_size + float(attackingframe["hitbox_2_size"]):
+                    return i
+                if distance3 < defender_size + float(attackingframe["hitbox_3_size"]):
+                    return i
+                if distance4 < defender_size + float(attackingframe["hitbox_4_size"]):
+                    return i
+        return 0
 
     # Returns a frame dict for the specified frame
     def getframe(self, character, action, action_frame):
