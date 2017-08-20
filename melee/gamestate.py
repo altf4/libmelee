05a44d5bd86abd6c89d3008dca30a9f8041ba19d
AltF4 <altf4@phx2600.org>
2017-08-20 11:35:58 -0700

    Fix off_stage bool to take height into account.  - You could be beneath the stage and count as "on" it

--- melee/gamestate.py
+++ melee/gamestate.py
@@ -130,7 +130,8 @@ class GameState:
                 self.player[i].x = self.player[i].next_x
                 self.player[i].y = self.player[i].next_y
 
-                if abs(self.player[i].x) > stages.edgegroundposition(self.stage):
+                if abs(self.player[i].x) > stages.edgegroundposition(self.stage) and \
+                        self.player[i].y > -3:
                     self.player[i].off_stage = True
                 else:
                     self.player[i].off_stage = False
