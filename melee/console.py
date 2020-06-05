6d27ea54a77b0ba5cc3f4244ef1e3365902258e7
AltF4 <altf4petro@gmail.com>
2020-06-05 11:11:40 -0700

    Added missing "invulnerable" bool to player state

--- melee/console.py
+++ melee/console.py
@@ -296,6 +296,7 @@ class Console:
                 gamestate.player[controller_port].hitstun_frames_left = int(unpack(">f", event_bytes[0x2b:0x2b+4])[0])
                 gamestate.player[controller_port].on_ground = not bool(unpack(">B", event_bytes[0x2f:0x2f+1])[0])
                 gamestate.player[controller_port].jumps_left = unpack(">B", event_bytes[0x32:0x32+1])[0]
+                gamestate.player[controller_port].invulnerable = int(unpack(">B", event_bytes[0x34:0x34+1])[0]) != 0
 
                 event_bytes = event_bytes[event_size:]
                 continue
