d6a9a6cc158cdc5af95a973b656dbee9f58ccd60
AltF4 <altf4petro@gmail.com>
2020-06-19 23:52:45 -0700

    Removed requirements.txt file  - Setup has to happen elsewhere

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 setup(
-  name = 'melee',
-  packages = ['melee'],
-  version = '0.1.0',
-  description = 'Open API for making your own Smash Bros: Melee AI',
-  author = 'AltF4',
-  author_email = 'altf4@phx2600.org',
-  url = 'https://github.com/altf4/libmelee',
-  download_url = 'https://github.com/altf4/libmelee/tarball/0.1.0',
-  keywords = ['dolphin', 'AI', 'video games'],
-  classifiers = [],
-  license = "GPLv3",
-  include_package_data=True
+    name = 'melee',
+    packages = ['melee'],
+    version = '0.1.0',
+    description = 'Open API for making your own Smash Bros: Melee AI',
+    author = 'AltF4',
+    author_email = 'altf4petro@gmail.com',
+    url = 'https://github.com/altf4/libmelee',
+    download_url = 'https://github.com/altf4/libmelee/tarball/0.1.0',
+    keywords = ['dolphin', 'AI', 'video games', 'melee', 'smash bros'],
+    classifiers = [],
+    license = "LGPLv3",
+    include_package_data=True
 )
