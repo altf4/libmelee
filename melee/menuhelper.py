1170e199924ebcee0fc0e7e96b32e61e540ec920
AltF4 <altf4petro@gmail.com>
2020-07-09 15:29:29 -0700

    Fix for always selecting pokemon stadium

--- melee/menuhelper.py
+++ melee/menuhelper.py
@@ -50,9 +50,9 @@ class MenuHelper():
             MenuHelper.skip_postgame(controller=controller)
         # If we're at the stage select screen, choose a stage
         elif gamestate.menu_state == enums.Menu.STAGE_SELECT:
-            MenuHelper.choose_stage(stage=enums.Stage.POKEMON_STADIUM,
-                         gamestate=gamestate,
-                         controller=controller)
+            MenuHelper.choose_stage(stage=stage_selected,
+                                    gamestate=gamestate,
+                                    controller=controller)
         elif gamestate.menu_state == enums.Menu.MAIN_MENU:
             if connect_code:
                 MenuHelper.choose_direct_online(gamestate=gamestate, controller=controller)
