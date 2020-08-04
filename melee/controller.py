5135d0d5f1f2b3bb05f47190cec9e383bf63c60d
AltF4 <altf4petro@gmail.com>
2020-08-03 18:18:43 -0700

    Fix for windows pipe error  - woops

--- melee/controller.py
+++ melee/controller.py
@@ -296,7 +296,7 @@ class Controller:
         self.prev = copy.copy(self.current)
 
         if self._is_dolphin:
-            self.pipe.write("FLUSH\n")
+            self._write("FLUSH\n")
             if platform.system() != "Windows":
                 if not self.pipe:
                     return
