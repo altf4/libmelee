7e31681245bb6ff720d8341f7449fa264f1596b8
AltF4 <altf4petro@gmail.com>
2020-08-03 20:35:31 -0700

    On windows, retry connecting the controllers if it fails

--- melee/controller.py
+++ melee/controller.py
@@ -3,10 +3,12 @@
 import platform
 import sys
 import copy
+import time
 from struct import pack
 import serial
 try:
     import win32file
+    import pywintypes
 except ImportError:
     pass
 
@@ -94,17 +96,24 @@ class Controller:
 
             if self._is_dolphin:
                 if platform.system() == "Windows":
-                    # "Create File" in windows is what you use to open a file. Not
-                    #   create one. Because the windows API is stupid.
-                    self.pipe = win32file.CreateFile(
-                        self.pipe_path,
-                        win32file.GENERIC_WRITE,
-                        0,
-                        None,
-                        win32file.OPEN_EXISTING,
-                        0,
-                        None
-                    )
+                    # Windows can take a little while to actually make the pipes
+                    #   So keep trying a few times to connect to it
+                    for _ in range(5):
+                        try:
+                            # "Create File" in windows is what you use to open a file. Not
+                            #   create one. Because the windows API is stupid.
+                            self.pipe = win32file.CreateFile(
+                                self.pipe_path,
+                                win32file.GENERIC_WRITE,
+                                0,
+                                None,
+                                win32file.OPEN_EXISTING,
+                                0,
+                                None
+                            )
+                            return True
+                        except pywintypes.error:
+                            time.sleep(1)
                 else:
                     self.pipe = open(self.pipe_path, "w")
                 return True
@@ -282,7 +291,10 @@ class Controller:
         """ Platform independent button write function.
         """
         if platform.system() == "Windows":
-            win32file.WriteFile(self.pipe, command.encode())
+            try:
+                win32file.WriteFile(self.pipe, command.encode())
+            except pywintypes.error:
+                pass
         else:
             self.pipe.write(command)
 
