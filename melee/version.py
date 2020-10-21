bf484c4ac99d7351fd1f7c44e52c5f9a3ed9fb1b
AltF4 <altf4petro@gmail.com>
2020-10-20 20:20:57 -0700

    0.19.1

--- melee/version.py
+++ melee/version.py
@@ -1,2 +1,2 @@
 """ Version of libmelee """
-__version__ = '0.19.0'
+__version__ = '0.19.1'
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         'numpy',
         'pywin32; platform_system=="Windows"'
     ],
-    version = '0.19.0',
+    version = '0.19.1',
     description = 'Open API written in Python 3 for making your own Smash Bros: Melee AI that works with Slippi Online',
     author = 'AltF4',
     author_email = 'altf4petro@gmail.com',
