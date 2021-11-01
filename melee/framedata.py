995c45c690c4ce0ac73bfe0f4af5778e8490b303
AltF4 <altf4petro@gmail.com>
2021-10-31 17:12:41 -0700

    Add Mewtwo air side-b as a grab

--- melee/framedata.py
+++ melee/framedata.py
@@ -113,7 +113,7 @@ class FrameData:
             return True
 
         if character == Character.MEWTWO and \
-                action in [Action.SWORD_DANCE_2_MID]:
+                action in [Action.SWORD_DANCE_2_MID, Action.SWORD_DANCE_3_HIGH]:
             return True
 
         return False
